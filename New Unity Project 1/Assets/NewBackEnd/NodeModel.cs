18ea180b1bacffe0e5397ce6f526307d0229033f
kevincarpenterdev <kevin.carpenter.dev@gmail.com>
2015-11-14 20:45:55 -0600

    Started work on the "new" back end, nodes now directionally reference themselves in the editor

---
+++ New Unity Project 1/Assets/NewBackEnd/NodeModel.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NodeModel : MonoBehaviour {
+
+    [SerializeField] protected GameObject NorthNode;
+    [SerializeField] protected GameObject EastNode;
+    [SerializeField] protected GameObject WestNode;
+    [SerializeField] protected GameObject SouthNode;
+    //Testing
+    public bool hasVisited;
+
+	// Use this for initialization
+	void Start () {
+        hasVisited = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public GameObject GetNorth() {
+        if(NorthNode != null) {
+            return NorthNode;
+        } else {
+            return null;
+        }
+    }
+    public GameObject GetEast() {
+        if (EastNode != null) {
+            return EastNode;
+        } else {
+            return null;
+        }
+    }
+    public GameObject GetWest() {
+        if (NorthNode != null) {
+            return WestNode;
+        } else {
+            return null;
+        }
+    }
+    public GameObject GetSouth() {
+        if (NorthNode != null) {
+            return SouthNode;
+        } else {
+            return null;
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.tag == "Capsule") hasVisited = true;
+    }
+
+}
