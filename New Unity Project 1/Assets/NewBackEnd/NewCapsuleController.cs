80fe19d3011cb2f5a42fae248216eb7a47cf4c40
Landon <w0304105@selu.edu>
2015-11-20 13:59:12 -0600

    Triggers and checks
    New trigger for hit detection and control flow concept implemented to
    confirm maze solving capability.

--- New Unity Project 1/Assets/NewBackEnd/NewCapsuleController.cs
+++ New Unity Project 1/Assets/NewBackEnd/NewCapsuleController.cs
@@ -21,17 +21,53 @@ public class NewCapsuleController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(!isAtTarget) {
-            GoToTarget();
+        //if(!isAtTarget) {
+        //    GoToTarget();
+        //}
+       // Example: Traverses from one node to another
+       //Will cause a reference error
+       if (isAtTarget && target.isGoal == true)
+        {
+
         }
-       /* Example: Traverses from one node to another
-       Will cause a reference error
-        if (!isAtTarget && target != null) {
+        else if (!isAtTarget) {
             GoToTarget();
         } else { 
-            target = target.GetWest();
+            if (target.GetWest() != null && target.GetWest().hasVisited == false)
+            {
+                target = target.GetWest();
+            }
+            else if (target.GetNorth() != null && target.GetNorth().hasVisited == false)
+            {
+                target = target.GetNorth();
+            }
+            else if (target.GetEast() != null && target.GetEast().hasVisited == false)
+            {
+                target = target.GetEast();
+            }
+            else if (target.GetSouth() != null && target.GetSouth().hasVisited == false)
+            {
+                target = target.GetSouth();
+            }
+            else if (target.GetWest() != null)
+            {
+                target = target.GetWest();
+            }
+            else if (target.GetNorth() != null)
+            {
+                target = target.GetNorth();
+            }
+            else if (target.GetEast() != null)
+            {
+                target = target.GetEast();
+            }
+            else if (target.GetSouth() != null)
+            {
+                target = target.GetSouth();
+            }
+
             isAtTarget = false;
-        } */
+        }
 	}
 
     void GoToTarget() {
@@ -44,9 +80,16 @@ public class NewCapsuleController : MonoBehaviour {
         
     }
 
-    void OnColliderEnter(Collider other) {
-        if(other.tag == "Node") {
-            isAtTarget = true;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Node")
+        {
+            if (other.gameObject.GetInstanceID() == target.gameObject.GetInstanceID())
+            {
+                print("Target hit");
+                isAtTarget = true;
+                target.hasVisited = true;
+            }
         }
     }
 
