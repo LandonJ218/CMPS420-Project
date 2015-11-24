5f052d46a9dc5708eaddaec844371f3137559a16
kevincarpenterdev <kevin.carpenter.dev@gmail.com>
2015-11-23 23:33:01 -0600

    Filled out test cases, prepping for final reset button integration

--- New Unity Project 1/Assets/NewBackEnd/NewCapsuleController.cs
+++ New Unity Project 1/Assets/NewBackEnd/NewCapsuleController.cs
@@ -12,6 +12,7 @@ public class NewCapsuleController : MonoBehaviour {
     protected CharacterController CC;
 
     public bool isAtTarget;
+    public bool isTestCase;
 
     // Use this for initialization
     void Start () {
@@ -24,51 +25,46 @@ public class NewCapsuleController : MonoBehaviour {
         //if(!isAtTarget) {
         //    GoToTarget();
         //}
-       // Example: Traverses from one node to another
-       //Altered for new properties
-       if (isAtTarget && target.isGoal == true)
-        {
-
+        // Example: Traverses from one node to another
+        //Altered for new properties
+        if(isTestCase) {
+            testCase();
+        } else {
+            if(!isAtTarget) {
+                GoToTarget();
+            }
         }
-        else if (!isAtTarget) {
+        
+	}
+
+
+    void testCase() {
+        if (isAtTarget && target.isGoal == true) {
+
+        } else if (!isAtTarget) {
             GoToTarget();
-        } else { 
-            if (target.GetWest() != null && target.GetWest().getProperty("HasVisited") == false)
-            {
+        } else {
+            if (target.GetWest() != null && target.GetWest().getProperty("HasVisited") == false) {
                 target = target.GetWest();
-            }
-            else if (target.GetNorth() != null && target.GetNorth().getProperty("HasVisited") == false)
-            {
+            } else if (target.GetNorth() != null && target.GetNorth().getProperty("HasVisited") == false) {
                 target = target.GetNorth();
-            }
-            else if (target.GetEast() != null && target.GetEast().getProperty("HasVisited") == false)
-            {
+            } else if (target.GetEast() != null && target.GetEast().getProperty("HasVisited") == false) {
                 target = target.GetEast();
-            }
-            else if (target.GetSouth() != null && target.GetSouth().getProperty("HasVisited") == false)
-            {
+            } else if (target.GetSouth() != null && target.GetSouth().getProperty("HasVisited") == false) {
                 target = target.GetSouth();
-            }
-            else if (target.GetWest() != null)
-            {
+            } else if (target.GetWest() != null) {
                 target = target.GetWest();
-            }
-            else if (target.GetNorth() != null)
-            {
+            } else if (target.GetNorth() != null) {
                 target = target.GetNorth();
-            }
-            else if (target.GetEast() != null)
-            {
+            } else if (target.GetEast() != null) {
                 target = target.GetEast();
-            }
-            else if (target.GetSouth() != null)
-            {
+            } else if (target.GetSouth() != null) {
                 target = target.GetSouth();
             }
 
             isAtTarget = false;
         }
-	}
+    }
 
     void GoToTarget() {
         Transform NodePos = target.GetTransform();
