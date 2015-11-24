bea0887220a4687a8a5284aa6917bdf45f75c805
kevincarpenterdev <kevin.carpenter.dev@gmail.com>
2015-11-24 14:09:23 -0600

    Early work on Instruction translation

---
+++ New Unity Project 1/Assets/UI/UI Scripts/InstructionsHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class InstructionsHandler : MonoBehaviour {
+
+    public List<GameObject> Instructions = new List<GameObject>();
+
+
+    public void RemoveIns() {
+
+    }
+
+    public void GetINS(int i) {
+        //Instructions.IndexOf(i)
+        //Grab the stuff
+        //Return the struct
+    }
+
+}
