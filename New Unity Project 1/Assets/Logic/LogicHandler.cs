8f4d9e6953e080c0714aa8b911b80bd33765a177
Landon <w0304105@selu.edu>
2015-11-25 13:39:14 -0600

    Added LogicHandler
    LogiceHandler will be used to convert the list of user provided
    instructions into an array of structs that will be used to execute
    specific code blocks.

---
+++ New Unity Project 1/Assets/Logic/LogicHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+public class LogicHandler : MonoBehaviour {
+
+    struct behavior
+    {
+        public string direction;
+        public string property;
+        public string action;
+    }
+
+    behavior[] instructionList;
+
+	void loadInstructionList (List <GameObject> userInstructions) {
+        instructionList = new behavior[userInstructions.Count];
+        for(int i = 0; i < userInstructions.Count; i++)
+        {
+            // The userInstruction references do not currently exist
+            //instructionList[i].direction = userInstructions.ElementAt(i).iDirection;
+            //instructionList[i].property = userInstructions.ElementAt(i).iProperty;
+            //instructionList[i].action = userInstructions.ElementAt(i).iAction;
+        }
+	}
+	
+}
