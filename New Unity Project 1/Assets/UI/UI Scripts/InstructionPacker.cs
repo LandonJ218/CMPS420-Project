b2beaf1e51bb121817671fc79998c9d62d8e74c0
kevincarpenterdev <kevin.carpenter.dev@gmail.com>
2015-11-25 18:41:14 -0600

    Some UI cleanup and Event text on the UI, also some handy dandy setup for packing instructions!

--- New Unity Project 1/Assets/UI/UI Scripts/InstructionPacker.cs
+++ New Unity Project 1/Assets/UI/UI Scripts/InstructionPacker.cs
@@ -3,27 +3,32 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class InstructionPacker : MonoBehaviour {
+
+    //UI stuff
     public Dropdown Direction;
     public Dropdown Check;
     public Text EventName;
     public InstructionsHandler IH;
 
+    //Data Stuff
+    public string TargetNode;
+    public string TargetCondition;
+
     void OnGUI() {
-       
+        //TargetNode
+        TargetNode = Direction.captionText.text;
+        TargetCondition = Check.captionText.text;
+        if (TargetCondition == "DeadEnd") {
+            EventName.text = "CheckDE";
+        } else if(TargetNode == "Current" && (TargetCondition == "Visited" || TargetCondition == "NotVisited" || TargetCondition == "Goal")) {
+            EventName.text = "No Action"; 
+        } else {
+            EventName.text = "Move";
+        }
     }
 
     public void deleteThis() {
         IH.RemoveIns(gameObject);
         Destroy(gameObject);
     }
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
